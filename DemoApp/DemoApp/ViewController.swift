9d716872cee01a273e1d0af3a6ae62870b839ca8
dolarlayang <iamdolarlayang@gmail.com>
2023-06-05 17:17:23 -0400

    Changes to DemoApp

--- DemoApp/DemoApp/ViewController.swift
+++ DemoApp/DemoApp/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var frontPageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        frontPageLabel.text = "ThisIsSupposedtoBeMortgage"
     }
+    
 
 
 }
