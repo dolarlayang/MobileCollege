1a6c4af22abf5f09296d3ca4da02dd0c18caae22
dolarlayang <iamdolarlayang@gmail.com>
2023-06-13 15:33:43 -0400

    Homework Files

---
+++ completedAssignments/MortgageCalculator2/MortgageCalculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MortgageCalculator
+//
+//  Created by mac07 on 6/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mortgageCalc: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mortgageCalc.text = "Mortgage Caclulator"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
